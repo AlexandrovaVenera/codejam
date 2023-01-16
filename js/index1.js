a18d41a2c7b883488f59c0d5b21ca664863e592e
Faizieva Venera <v.faizyeva@gmail.com>
2023-01-17 01:27:51 +0300

    fix: added new structure

--- js/index1.js
+++ js/index1.js
@@ -1,11 +1,14 @@
 
-let puzzle = document.getElementById("puzzle");
+let containerNode = document.getElementById("puzzle");
 let framesSize = Array.from(document.querySelectorAll(".frameItem"));
 let frame = document.getElementById("current-frame");
 let itemNodes = Array.from(document.querySelectorAll('.item'))
-
 frame.textContent = "4x4";
 let moveCount = 0;
+
+
+
+ 
 
 
 /*Количество пятнашек */
@@ -29,9 +32,24 @@ setPositionItems(matrix);
 /*---------------------------*/
 
 /*Перемешать пятнашки*/
-document.getElementById("shuffle").addEventListener('click',()=>{
-  const flatMatrix = matrix.flat();
-})
+document.getElementById("shuffle").addEventListener('click', shuffle)
+/*-------------------------- */
+
+/*Позиция по клику*/
+const blankNumber = 16 //пустой элемент
+containerNode.addEventListener('click', (event)=>{
+  const buttonNode = event.target.closest('button');
+  if(!buttonNode){
+    return
+  }
+  const buttonNumber = Number(buttonNode.dataset.matrixId)
+  const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix)
+  const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+  const isValid = isValidForSwap(buttonCoords, blankCoords);
+  if (isValid) {
+      swap(blankCoords, buttonCoords,matrix)
+      setPositionItems(matrix)
+}})
 
 
 /*Helpers */
@@ -71,3 +89,121 @@ function setNodeStyles(node,x,y){
 }
 
 /*-------------------*/
+
+function shuffleArray(arr){
+return arr.map(value=>({value, sort:Math.random()})).sort((a,b)=>a.sort-b.sort).map(({value})=>value)
+}
+
+/*Находим координаты элемента */
+function findCoordinatesByNumber(number, matrix){
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] == number) {
+        return { x, y };
+      }
+    }
+  }
+  return null;
+}
+/*-------------------*/
+/*Валидация */
+function isValidForSwap(coord1, coord2) {
+  const diffX = Math.abs(coord1.x - coord2.x);
+  const diffY = Math.abs(coord1.y - coord2.y);
+  return (
+    (diffX === 1 || diffY === 1) &&
+    (coord1.x === coord2.x || coord1.y === coord2.y) &&
+    !document.getElementById("stop").classList.contains("active")
+  );
+}
+/*----------------------*/
+
+/*Замена координат элемента при перемещении */
+function swap(coords1, coords2, matrix, moveCount) {
+  const coords1Number = matrix[coords1.y][coords1.x];
+  matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
+  matrix[coords2.y][coords2.x] = coords1Number;
+  if(isWon(matrix)){
+    addWonClass();
+  }
+}
+/*-----------------------*/
+/*Проверка массива на выйгрыш */
+const winFlatArray = new Array(16).fill(0).map((el,i)=>el=i+1)
+console.log(winFlatArray)
+function isWon(matrix){
+  const flatMatrix = matrix.flat();
+  for(let i=0; i<winFlatArray.length; i++){
+    if(flatMatrix[i]!==winFlatArray[i]){
+      return false
+    }
+  }
+return true
+}
+/*------------------------*/
+
+/*В случае выйгрыша*/
+let win = document.querySelector(".win");
+function addWonClass(){
+  win.classList.add("popup");
+  win.innerHTML=`
+  <div class="win-container">Hooray! You solved the puzzle in:</div>
+  <button class="win-button">OK</button>
+  `
+  const containerWin = document.querySelector(".win-container")
+  // containerWin.append(("0" + hours).slice(-2));
+  // containerWin.append(":");
+  // containerWin.append(("0" + minutes).slice(-2));
+  // containerWin.append(":");
+  // containerWin.append(("0" + seconds).slice(-2));
+  // containerWin.append(" and ");
+  // containerWin.append(moveCount);
+  // containerWin.append(" moves");
+
+  const winBtn = document.querySelector('.win-button')
+  winBtn.addEventListener('click', goBack)
+}
+
+/*Вернуться обратно в игру */
+function goBack(){
+  win.classList.remove("popup");
+}
+
+/*--------------*/
+/** */
+// function shuffle(){
+//     const shuffledArray = shuffleArray(matrix.flat());
+//     console.log(shuffledArray)
+//     matrix = getMatrix(shuffledArray)
+//     setPositionItems(matrix);
+  
+// }
+
+function shuffle(){
+randomSwap(matrix)
+setPositionItems(matrix)
+}
+
+function randomSwap(matrix){
+  const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+  const validCoords = findValidCoords({
+    blankCoords,
+    matrix
+  })
+  console.log( validCoords)
+  const swapCoords = validCoords[Math.floor(Math.random()* validCoords.length)];
+  swap(blankCoords, swapCoords,matrix)
+}
+
+
+function findValidCoords({blankCoords,matrix} ){
+  const validCoords = [];
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if(isValidForSwap({x,y}, blankCoords)){
+      validCoords.push({x,y})
+      }
+    }
+  }
+  return
+}
